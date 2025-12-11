05483f4f6fd6116affc0dbc95e13de4dbab7418c
Felipe-Marcos-De-Souza <Felipinhomarcos582@gmail.com>
2025-12-11 16:01:34 -0300

    Enhance database connection with session and creation
    Added session management and database creation function.

--- database.py
+++ database.py
@@ -1,14 +1,20 @@
 from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from models import Base
 
-# Conexão com o servidor:
 DB_URL = "mysql+pymysql://root:@localhost:5506/consulta_fisioterapia?charset=utf8mb4"
+
 engine = create_engine(DB_URL, echo=True)
 
-# Testar conexão de verdade
+SessionLocal = sessionmaker(bind=engine)
+
+def criar_banco():
+    Base.metadata.create_all(engine)
+
+# Teste de conexão
 try:
     with engine.connect() as conn:
         conn.execute(text("SELECT 1"))
     print("O Banco está conectado com sucesso!")
 except Exception as e:
-
     print("Erro ao conectar:", e)
