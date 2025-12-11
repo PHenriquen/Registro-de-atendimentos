b701a0a48d6f5fc2a4983b947b8f449e47cd1892
Felipe-Marcos-De-Souza <Felipinhomarcos582@gmail.com>
2025-12-11 14:52:45 -0300

    Atualizada o nome e porta do banco

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 
 # Conexão com o servidor:
-DB_URL = "mysql+pymysql://root:@localhost:3308/taskflow_db?charset=utf8mb4"
+DB_URL = "mysql+pymysql://root:@localhost:5506/consulta_fisioterapia?charset=utf8mb4"
 engine = create_engine(DB_URL, echo=True)
 
 # Testar conexão de verdade
@@ -10,4 +10,5 @@ try:
         conn.execute(text("SELECT 1"))
     print("O Banco está conectado com sucesso!")
 except Exception as e:
+
     print("Erro ao conectar:", e)
